59f5d69dc9f3065d9e11dd4be8ac387330a2336b
LLseven <382269727@qq.com>
2015-10-06 03:18:49 +0800

    付款

---
+++ src/com/zy/personal/dao/MemOrderDao.java
@@ -0,0 +1,8 @@
+package com.zy.personal.dao;
+
+import com.zy.common.dao.CommonDao;
+import com.zy.personal.entity.MemOrder;
+
+public interface MemOrderDao extends CommonDao<MemOrder, String> {
+
+}
